35624ae60b9db70e66592bbae79d85687407c9ed
TimLedger <tim_ledger@mail.ru>
2024-01-30 11:56:44 +0600

    добавлена возможность отправлять сообщения

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 const ChatMessages: React.FC = () => {
   const [messages, setMessages] = useState([]);
+  const [messageText, setMessageText] = useState('');
+  const [authorName, setAuthorName] = useState('Tim Ledger');
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -18,17 +20,57 @@ const ChatMessages: React.FC = () => {
         throw new Error('Не удалось получить сообщения.');
       }
       const data = await response.json();
-      setMessages(data);
+      setMessages(data.reverse());
     } catch (error) {
       console.error('Ошибка при получении сообщений:', error);
     }
   };
 
-  
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const url = 'http://146.185.154.90:8000/messages';
+      const data = new URLSearchParams();
+      data.set('message', messageText);
+      data.set('author', authorName);
+
+      const response = await fetch(url, {
+        method: 'POST',
+        body: data
+      });
+
+      if (!response.ok) {
+        throw new Error('Не удалось отправить сообщение');
+      }
+
+      console.log('Сообщение успешно отправлено');
+      setMessageText('');
+      fetchMessages(); 
+    } catch (error) {
+      console.error('Ошибка отправки сообщения:', error);
+    }
+  };
 
   return (
     <div>
       <h2>Чат</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={messageText}
+          onChange={(e) => setMessageText(e.target.value)}
+          placeholder="Введите сообщение..."
+          required
+        />
+        <input
+          type="text"
+          value={authorName}
+          onChange={(e) => setAuthorName(e.target.value)}
+          placeholder="Ваше имя..."
+          required
+        />
+        <button type="submit">Отправить</button>
+      </form>
       <ul>
         {messages.map((message: any) => (
           <li key={message._id}>
